1cefedd36cb5a7fc9f72cdec6ffd5aa88906e055
Tou Fue Yang <touyangbiz@gmail.com>
2022-11-22 11:30:04 -0600

    creating the shell

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,1 +1,75 @@
+// Take in the album's title, artist, yearPublished as input parameters
+// Create a new object having the above properties
+// Add the new object to the end of the collection array
+// Return the newly created object
+
 console.log('***** Music Collection *****')
+let collection = [];
+
+function addTOCollection(titles, artist, yearPublieshed){
+
+};
+
+
+let ablumsInfo = {
+    title: ,
+    artist: ,
+    yearPublieshed: ,
+}
+
+// // Add a function named showCollection. This function should:
+// // Take in an array parameter. (This allows it to be reused to show any collection, 
+//   --------like the results from the find or search.)
+// // Console.log the number of items in the array.
+// // Loop over the array and console.log each album's information formatted like: 
+// ----------TITLE by ARTIST, published in YEAR
+
+function showCollection(collection){
+    console.log('we are in the showCollection array');
+}
+
+// Test the showCollection function.
+return showCollection(collection);
+
+
+
+
+
+
+
+
+albums1={
+    artist:
+    yearPublieshed:
+};
+
+
+
+albums2={
+    artist:
+    yearPublieshed:
+};
+
+
+album3s={
+    artist:
+    yearPublieshed:
+};
+
+
+albums4={
+    artist:
+    yearPublieshed:
+};
+
+
+albums5={
+    artist:
+    yearPublieshed:
+};
+
+
+albums6={
+    artist:
+    yearPublieshed:
+};
